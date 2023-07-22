340dea4dda8ab4b07749b464667d49b311c748f1
kguyer <kguyer@harding.edu>
2023-07-22 13:43:28 -0500

    Remove username column

--- db/schema.rb
+++ db/schema.rb
@@ -28,7 +28,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_07_22_181728) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "username"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
